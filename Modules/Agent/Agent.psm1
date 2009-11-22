7f1eaf05c3a7affb330d2c1b75489873490178b9
cmille19 <CMille19>
2009-11-22 21:41:57 +0000

    Created comment-based help for Agent module. Added Test.Agent.Help.ps1 to test scripts.

--- Modules/Agent/Agent.psm1
+++ Modules/Agent/Agent.psm1
@@ -13,6 +13,23 @@
 # ---------------------------------------------------------------------------
 
 #######################
+<#
+.SYNOPSIS
+Gets a JobServer.
+.DESCRIPTION
+The Get-AgentJobServer function  gets the JobServer specified. This is the top level object for SMO.Agent.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentJobServer
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.JobServer
+    Get-AgentJobServer returns a Microsoft.SqlServer.Management.Smo.Agent.JobServer object.
+.EXAMPLE
+Get-AgentJobServer "Z002\sql2K8"
+This command gets the JobServer for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentJobServer
+#>
 function Get-AgentJobServer
 {
     param(
@@ -34,6 +51,23 @@ function Get-AgentJobServer
 } #Get-AgentJobServer
 
 #######################
+<#
+.SYNOPSIS
+Gets an SMO.Agent AlertCategory object or collection of AlertCategory objects.
+.DESCRIPTION
+The Get-AgentAlertCategory function  gets  an SMO.Agent AlertCategory object or a collection of AlertCategory objects from the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentAlertCategory
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.AlertCategory
+    Get-AgentAlertCategory returns a Microsoft.SqlServer.Management.Smo.Agent.AlertCategory object.
+.EXAMPLE
+Get-AgentAlertCategory "Z002\sql2K8"
+This command gets a collection of AlertCategory objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentAlertCategory
+#>
 function Get-AgentAlertCategory
 {
     param(
@@ -63,6 +97,23 @@ function Get-AgentAlertCategory
 } #Get-AgentAlertCategory
 
 #######################
+<#
+.SYNOPSIS
+Gets an Alert object or collection of Alert objects.
+.DESCRIPTION
+The Get-AgentAlert function  gets an Alert object or a collection of Alert objects from the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentAlert
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.Alert
+    Get-AgentAlert returns a Microsoft.SqlServer.Management.Smo.Agent.Alert object.
+.EXAMPLE
+Get-AgentAlert "Z002\sql2K8"
+This command gets a collection of Alert objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentAlert
+#>
 function Get-AgentAlert
 {
     param(
@@ -92,6 +143,23 @@ function Get-AgentAlert
 } #Get-AgentAlert
 
 #######################
+<#
+.SYNOPSIS
+Gets an Job object or collection of Job objects.
+.DESCRIPTION
+The Get-AgentJob function  gets an Job object or a collection of Job objects from the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentJob
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.Job
+    Get-AgentJob returns a Microsoft.SqlServer.Management.Smo.Agent.Job object.
+.EXAMPLE
+Get-AgentJob "Z002\sql2K8"
+This command gets a collection of Job objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentJob
+#>
 function Get-AgentJob
 {
     param(
@@ -120,6 +188,24 @@ function Get-AgentJob
 } #Get-AgentJob
 
 #######################
+<#
+.SYNOPSIS
+Gets an JobSchedule object or collection of JobSchedule objects.
+.DESCRIPTION
+The Get-AgentJobSchedule function  gets an JobSchedule object or a collection of JobSchedule objects for the specified Job.
+.INPUTS
+None
+    You can pipe job Job to Get-AgentJobSchedule.
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.JobSchedule
+    Get-AgentJobSchedule returns a Microsoft.SqlServer.Management.Smo.Agent.JobSchedule object.
+.EXAMPLE
+Get-AgentJob "Z002\sql2K8" | Get-AgentJobSchedule
+This command gets a collection of JobSchedule objects for all jobs on SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentJobSchedule
+Get-AgentJob
+#>
 function Get-AgentJobSchedule
 {
     param(
@@ -143,6 +229,24 @@ function Get-AgentJobSchedule
 } #Get-AgentJobSchedule
 
 #######################
+<#
+.SYNOPSIS
+Gets an JobStep object or collection of JobStep objects.
+.DESCRIPTION
+The Get-AgentJobStep function  gets an JobStep object or a collection of JobStep objects for the specified Job.
+.INPUTS
+None
+    You can pipe job Job to Get-AgentJobStep.
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.JobStep
+    Get-AgentJobStep returns a Microsoft.SqlServer.Management.Smo.Agent.JobStep object.
+.EXAMPLE
+Get-AgentJob "Z002\sql2K8" | Get-AgentJobStep
+This command gets a collection of JobStep objects for all jobs on SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentJobStep
+Get-AgentJob
+#>
 function Get-AgentJobStep
 {
     param(
@@ -166,6 +270,23 @@ function Get-AgentJobStep
 } #Get-AgentJobStep
 
 #######################
+<#
+.SYNOPSIS
+Gets an AgentOperator object or collection of AgentOperator objects.
+.DESCRIPTION
+The Get-AgentOperator function  gets an Operator object or a collection of Operator objects for the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentOperator
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.Operator
+    Get-AgentOperator returns a Microsoft.SqlServer.Management.Smo.Agent.Operator object.
+.EXAMPLE
+Get-AgentOperator "Z002\sql2K8"
+This command gets a collection of Operator objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentOperator
+#>
 function Get-AgentOperator
 {
     param(
@@ -194,6 +315,23 @@ function Get-AgentOperator
 } #Get-AgentOperator
 
 #######################
+<#
+.SYNOPSIS
+Gets an OperatorCategory object or collection of OperatorCategory objects.
+.DESCRIPTION
+The Get-AgentOperatorCategory function  gets an OperatorCategory object or a collection of OperatorCategory objects for the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentOperatorCategory
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.OperatorCategory
+    Get-AgentOperatorCategory returns a Microsoft.SqlServer.Management.Smo.Agent.OperatorCategory object.
+.EXAMPLE
+Get-AgentOperatorCategory "Z002\sql2K8"
+This command gets a collection of OperatorCategory objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentOperatorCategory
+#>
 function Get-AgentOperatorCategory
 {
     param(
@@ -222,6 +360,23 @@ function Get-AgentOperatorCategory
 } #Get-AgentOperatorCategory
 
 #######################
+<#
+.SYNOPSIS
+Gets an ProxyAccount object or collection of ProxyAccount objects.
+.DESCRIPTION
+The Get-AgentProxyAccount function  gets an ProxyAccount object or a collection of ProxyAccount objects for the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentProxyAccount
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.ProxyAccount
+    Get-AgentProxyAccount returns a Microsoft.SqlServer.Management.Smo.Agent.ProxyAccount object.
+.EXAMPLE
+Get-AgentProxyAccount "Z002\sql2K8"
+This command gets a collection of ProxyAccount objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentProxyAccount
+#>
 function Get-AgentProxyAccount
 {
     param(
@@ -250,6 +405,26 @@ function Get-AgentProxyAccount
 } #Get-AgentProxyAccount
 
 #######################
+<#
+.SYNOPSIS
+Gets an JobSchedule object or collection of JobSchedule objects for JobServer Shared Schedules.
+.DESCRIPTION
+The Get-AgentSchedule function  gets an JobSchedule object or a collection of JobSchedule objects for JobServer Shared Schedules on the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentSchedule
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.JobSchedule
+    Get-AgentSchedule returns a Microsoft.SqlServer.Management.Smo.Agent.JobSchedule object.
+.NOTES
+The Get-AgentSchedule function differs from Get-AgentJobSchedule in that Get-AgentSchedule is used for shared server-level schedules. In most cases you will want to use Get-AgentJobSchedule rather than this one.
+.EXAMPLE
+Get-AgentSchedule "Z002\sql2K8"
+This command gets a collection of JobSchedule objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentSchedule
+Get-AgentJobSchedule
+#>
 function Get-AgentSchedule
 {
     param(
@@ -278,6 +453,23 @@ function Get-AgentSchedule
 } #Get-AgentSchedule
 
 #######################
+<#
+.SYNOPSIS
+Gets an TargetServerGroup object or collection of TargetServerGroup objects.
+.DESCRIPTION
+The Get-AgentTargetServerGroup function  gets an TargetServerGroup object or a collection of TargetServerGroup objects for the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentTargetServerGroup
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.TargetServerGroup
+    Get-AgentTargetServerGroup returns a Microsoft.SqlServer.Management.Smo.Agent.TargetServerGroup object.
+.EXAMPLE
+Get-AgentTargetServerGroup "Z002\sql2K8"
+This command gets a collection of TargetServerGroup objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentTargetServerGroup
+#>
 function Get-AgentTargetServerGroup
 {
     param(
@@ -306,6 +498,23 @@ function Get-AgentTargetServerGroup
 } #Get-AgentTargetServerGroup
 
 #######################
+<#
+.SYNOPSIS
+Gets an TargetServer object or collection of TargetServer objects.
+.DESCRIPTION
+The Get-AgentTargetServer function  gets an TargetServer object or a collection of TargetServer objects for the specified server.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentTargetServer
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.TargetServer
+    Get-AgentTargetServer returns a Microsoft.SqlServer.Management.Smo.Agent.TargetServer object.
+.EXAMPLE
+Get-AgentTargetServer "Z002\sql2K8"
+This command gets a collection of TargetServer objects for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentTargetServer
+#>
 function Get-AgentTargetServer
 {
     param(
@@ -334,6 +543,24 @@ function Get-AgentTargetServer
 } #Get-AgentTargetServer
 
 #######################
+<#
+.SYNOPSIS
+Sets filtering option used in Get-AgentJobHistory function.
+.DESCRIPTION
+The Set-AgentJobHistoryFilter function  gets an JobHistoryFilter object that then can be used by the Get-AgentJobHistory function.
+.INPUTS
+None
+    You cannot pipe objects to Set-AgentJobHistoryFilter
+.OUTPUTS
+Microsoft.SqlServer.Management.Smo.Agent.JobHistoryFilter
+    Set-AgentJobHistoryFilter returns a Microsoft.SqlServer.Management.Smo.Agent.JobHistoryFilter object.
+.EXAMPLE
+Get-AgentJobHistory "Z002\sql2k8" $(Set-AgentJobHistoryFilter -outcome 'Failed')
+This command gets the job history for all failed jobs for SQL Server Z002\SQL2K8.
+.LINK
+Set-AgentJobHistoryFilter
+Get-AgentJobHistory
+#>
 function Set-AgentJobHistoryFilter
 {
     param(
@@ -353,6 +580,24 @@ function Set-AgentJobHistoryFilter
     Write-Output $jobHistoryFilter
 }
 #######################
+<#
+.SYNOPSIS
+Gets job history.
+.DESCRIPTION
+The Get-AgentJobHistory function  gets Job History for the specified server. Filtering can be applied by using the Set-AgentJobHistoryFilter function.
+.INPUTS
+None
+    You cannot pipe objects to Get-AgentJobHistory
+.OUTPUTS
+System.Data.DataRow
+    Get-AgentJobHistory returns a System.Data.DataRow
+.EXAMPLE
+Get-AgentJobHistory "Z002\sql2k8" $(Set-AgentJobHistoryFilter -outcome 'Failed')
+This command gets the job history for all failed jobs for SQL Server Z002\SQL2K8.
+.LINK
+Get-AgentJobHistory
+Set-AgentJobHistoryFilter
+#>
 function Get-AgentJobHistory
 {
     param(
